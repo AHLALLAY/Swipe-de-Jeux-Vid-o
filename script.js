4124e2b4f029b582579dea4f9975b2cba459d8f6
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2024-11-05 00:20:50 +0100

    j'ai commencer à défiir la logique avec laquelle je vais effectuer les opération de glissement avec succes

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const carteJeux =  document.getElementById("carte-jeux");
+const cards =  document.querySelectorAll(".game-card");
+const likedArea = document.querySelector(".like-area");
+const dislikeedArea = document.querySelector(".dislike-area");
+const likedCardsContainer = document.querySelector(".card-store");
+// Ajouter l'evennement du glissement
+cards.forEach(card => {
+    card.addEventListener("dragStart", dragStart);
+});
+function dragStart(event){
+    //essayant de definir l'Id de la carte glissée comme Data transfereée
+    event.dataTransfer.setData("text/plain", event.target.id);
+}
+//Pour autoriser le glissement
+likedArea.addEventListener("dragover", dragOver);
+dislikeedArea.addEventListener("dragover", dragOver);
+
+function dragOver(event){
+    event.preventDefault();
+}
+// --- To be continued, I'm So Tired
